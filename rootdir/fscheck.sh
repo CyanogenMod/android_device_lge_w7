f39b47cd52e5e22d152ecabf566c63484e96fc2e
BigMajster <maszlij11@gmail.com>
2015-01-11 16:25:14 +0100

    Fix many problems in D415
    Change-Id: Id8f03903506688e019f2a7577faf2231201cdb21

--- rootdir/fscheck.sh
+++ rootdir/fscheck.sh
@@ -1,6 +1,6 @@
 #!/sbin/bbx sh
 
-FSTYPE=`/sbin/bbx blkid /dev/block/mmcblk0p34 | /sbin/bbx cut -d ' ' -f3 | /sbin/bbx cut -d '"' -f2`
+FSTYPE=`/sbin/bbx blkid /dev/block/platform/msm_sdcc.1/by-name/userdata | /sbin/bbx cut -d ' ' -f3 | /sbin/bbx cut -d '"' -f2`
 
 if [ "$FSTYPE" == "ext4" ]
 then
