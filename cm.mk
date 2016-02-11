ebcb671cf666c7072221779987d2147fafbbf0d7
Nikita <nikich340@gmail.com>
2016-02-12 08:15:42 +1000

    Drop full_<device>.mk
    Change-Id: I9f1432fc3dd0121897ce928556248f11241be85b

--- cm.mk
+++ cm.mk
@@ -1,5 +1,7 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/lge/w7/device_w7.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -8,7 +10,7 @@ TARGET_SCREEN_HEIGHT := 960
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
 PRODUCT_NAME := cm_w7
-PRODUCT_MANUFACTURER := lge
 PRODUCT_MODEL := w7
-
-$(call inherit-product, device/lge/w7/full_w7.mk)
+PRODUCT_DEVICE := w7
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_BRAND := lge
